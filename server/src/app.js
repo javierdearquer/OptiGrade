377d565f2a1e5340b5246a598911533a46d2305c
javierdearquer <javierdearquer@gmail.com>
2024-09-24 14:23:23 +0200

    removed dependencies form github

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,11 @@
 // In this file will be defined the algorithm 
-console.log('hello')
 const express = require('express') 
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
