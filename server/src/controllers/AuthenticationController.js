acc5511e45e73035bef51be8634813e2b0572422
javierdearquer <javierdearquer@gmail.com>
2024-09-26 18:48:10 +0200

    login hasing and checking completed

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,11 @@ module.exports = {
     async register (req, res) {
         try { 
             const user = await User.create(req.body)
-            res.send(user.toJSON())
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson)
+            })
         } catch (error) {
             res.status(400).send({
                 error: 'This email account is already in use'
@@ -31,14 +35,14 @@ module.exports = {
             })
 
             if (!user) {
-                res.status(403).send({
+                return res.status(403).send({
                     error: 'The login information was incorrect'
                 })
             }
 
-            const isPasswordValid = password == user.password
+            const isPasswordValid = await user.comparePassword(password)
             if (!isPasswordValid) {
-                res.status(403).send({
+                return res.status(403).send({
                     error: 'The login information was incorrect'
                 })
             }
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,8 +1,41 @@
-module.exports = (sequelize,DataTypes) =>
-    sequelize.define('User', {
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+function hashPassword(user,options){
+    const SALT_FACTOR = 8
+    if (!user.changed('password')){
+        return;
+    }
+    //console.log('password hashed')
+    return bcrypt
+        .genSaltAsync(SALT_FACTOR)
+        .then(salt => bcrypt.hashAsync(user.password, salt, null))
+        .then(hash => {
+            user.setDataValue('password', hash)
+        })
+}
+
+module.exports = (sequelize,DataTypes) => {
+    const User = sequelize.define('User', {
         email: {
             type: DataTypes.STRING,
             unique: true
         },
         password: DataTypes.STRING
+    },{
+        hooks:{
+            //beforeCreate: hashPassword,
+            beforeUpdate: hashPassword,
+            beforeSave: hashPassword
+        }
     })
+
+    User.prototype.comparePassword = async function (password) {
+        //console.log(password, this.password)
+        const isMatch = await bcrypt.compareAsync(password, this.password)
+        //console.log(isMatch)
+        return isMatch
+    }
+
+    return User
+}
